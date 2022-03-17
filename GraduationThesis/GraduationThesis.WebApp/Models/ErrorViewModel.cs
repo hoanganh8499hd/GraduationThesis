bb609256917f5f3d7a1ff5312e35268909b9bdeb
hoanganh8499hd <hoanganhpham.rasta@gmail.com>
2022-03-17 22:45:34 +0700

    crerate new project

---
+++ GraduationThesis/GraduationThesis.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GraduationThesis.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
